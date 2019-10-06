49d8d6c8b6fd73591030c0c2c041c42ca91ceb91
Smrutidash22 <55426746+Smrutidash22@users.noreply.github.com>
2019-10-06 11:50:51 -0700

    PCA

---
+++ src/dimReduction/PCA.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.sparse.linalg import svds
+
+
+def calcPCA(data,kv):
+    cov_data = np.cov(data)
+    u,s,v = svds(cov_data, k=kv, which ='LM')
+    s = np.diag(s)
+    u = np.flip(u,axis=1)
+    s = np.flip(s)
+    v = np.flip(v,axis=0) 
+    return u,s,v
