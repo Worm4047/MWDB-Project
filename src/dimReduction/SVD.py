6aa5242eb7b4d9b6c2793085c6a8991f6216a737
Deepika2514 <msdeepikap06@gmail.com>
2019-10-05 21:01:21 -0700

    Pushing for SVD.

--- src/dimReduction/SVD.py
+++ src/dimReduction/SVD.py
@@ -1,10 +1,11 @@
 from src.dimReduction.interfaces.ReductionModel import ReductionModel
+import numpy as np
+
 
 class SVD(ReductionModel):
     def __init__(self, dataMatrix):
         super(SVD, self).__init__(dataMatrix)
 
     def getDecomposition(self):
-        #Implement the function here
-
-        print("Boom")
+        u, s, vt = np.linalg.svd(self.dataMatrix, full_matrices=False)
+        return u, s, vt
