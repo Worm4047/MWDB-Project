9e17b184f583ec658437d26503257eaea1550542
Smrutisudhadash <sudha.1922@gmail.com>
2019-10-18 16:35:00 -0700

    Task 6 & 7

--- src/dimReduction/PCA.py
+++ src/dimReduction/PCA.py
@@ -1,11 +1,21 @@
 import numpy as np
 from scipy.sparse.linalg import svds
+from sklearn.decomposition import PCA
+
+
 
 def calcPCA(data,kv):
+    """print("data = ", data.shape)
     cov_data = np.cov(data)
+    print("covv shape = ", cov_data.shape)
     u,s,v = svds(cov_data, k=kv, which ='LM')
+    print("v shape = ", v.shape)
     s = np.diag(s)
     u = np.flip(u,axis=1)
     s = np.flip(s)
-    v = np.flip(v,axis=0) 
-    return u,s,v
+    v = np.flip(v,axis=0) """
+    pca = PCA(n_components=kv)
+    u = pca.fit_transform(data)
+    v = pca.components_
+    return u,v
+
