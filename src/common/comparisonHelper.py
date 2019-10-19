1d74ae639a3bd20cf3b4feb15a56880df69e7f75
Sreekesh K C <sreekesh@asu.edu>
2019-10-19 01:13:23 -0700

    Modified: Cosine Computing and siilarity function

--- src/common/comparisonHelper.py
+++ src/common/comparisonHelper.py
@@ -3,16 +3,17 @@ import numpy as np
 
 
 # Cosine distance is calculated for 2 values queryImageVector and imageVector
+from scipy import spatial
+
+
 def compareWithCosine(queryImageVector, imageVector):
-    return np.linalg.norm(imageVector - queryImageVector)
+    # return np.linalg.norm(imageVector - queryImageVector)
+    return 1 - spatial.distance.cosine(imageVector, queryImageVector)
 
 
 # Eucledian distance is calculated for 2 values val1 and val2
 def computeWithEucledian(val1, val2):
-    sum = 0
-    for v1, v2 in zip(val1, val2):
-        sum += (v1 - v2) * (v1 - v2)
-    return np.math.sqrt(sum)
+    return np.linalg.norm(val1 - val2)
 
 
 # This function takes as input an image and a value K
@@ -20,19 +21,20 @@ def computeWithEucledian(val1, val2):
 # We are using a minHeap to store the values after comparing.
 # If the value of the min heap is more than k, we are removing the last element from the min heap
 # This function then returns the final K vectors and the
-def getMSimilarImages(dataMatrix, m, query_image_features, imageNames):
+def getMSimilarImages(dataMatrix, query_image_features, m, imageNames):
     h = []
     for row in range(0, dataMatrix.shape[0]-1):
         image_name = imageNames[row]
         image_vector = dataMatrix[row]
         # falttenedValue = [float(x) for x in s[1: -1].split(',')]
         # we are taking the negative value of the distance as there is no min-heap in python
-        distance = -compareWithCosine(image_vector, query_image_features)
+        distance = -computeWithEucledian(image_vector, query_image_features)
         heapq.heappush(h, (distance, image_name))
+        print(len(h), m)
         if len(h) > m:
             heapq.heappop(h)
     res = heapq.nlargest(m, h)
-    finalRes = []
+    finalRes = {}
     for item in res:
-        finalRes.append((-item[0], item[1]))
+        finalRes[item[1]] = -item[0]
     return finalRes
