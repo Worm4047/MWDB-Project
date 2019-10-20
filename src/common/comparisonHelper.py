14972d50f9881e3db875361cd626c1876a93bb68
Sreekesh K C <sreekesh@asu.edu>
2019-10-19 18:38:07 -0700

    Modified: Cosine similarity and euclidean distance

--- src/common/comparisonHelper.py
+++ src/common/comparisonHelper.py
@@ -15,6 +15,11 @@ def compareWithCosine(queryImageVector, imageVector):
 def computeWithEucledian(val1, val2):
     return np.linalg.norm(val1 - val2)
 
+#
+# # Eucledian distance is calculated for 2 values val1 and val2
+# def computeWithManhattan(val1, val2):
+#     return np.linalg.norm(val1 - val2)
+
 
 # This function takes as input an image and a value K
 # We then iterate through the values in the CSV file and compute the distance of each image vector from the input image
@@ -23,7 +28,6 @@ def computeWithEucledian(val1, val2):
 # This function then returns the final K vectors and the
 def getMSimilarImages(dataMatrix, query_image_features, m, imageNames):
     h = []
-    print(imageNames[0])
     for row in range(0, dataMatrix.shape[0]-1):
         image_name = imageNames[row]
         image_vector = dataMatrix[row]
@@ -33,6 +37,7 @@ def getMSimilarImages(dataMatrix, query_image_features, m, imageNames):
         heapq.heappush(h, (distance, image_name))
         if len(h) > m:
             heapq.heappop(h)
+    print(h)
     res = heapq.nlargest(m, h)
     finalRes = {}
     for item in res:
