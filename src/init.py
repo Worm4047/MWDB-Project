ee0f77491bb985a0560dc56d66ac22db76107d50
Vishnu Teja Yalakuntla <yvtheja@vishnus-mbp.3500.dhcp.asu.edu>
2019-11-30 18:05:38 -0700

    Small fixes in init

--- src/init.py
+++ src/init.py
@@ -11,17 +11,19 @@ from src.constants import CLASSIFICATION_IMG_DIR, CLASSIFICATION_META_CSV
 import glob
 
 def init():
-    ImageIndex()
-    imageDir = "/Users/yvtheja/Documents/Dataset2"
-    imagePaths = [
-        "/Users/yvtheja/Documents/Dataset2/Hand_0006321.jpg",
-        "/Users/yvtheja/Documents/Dataset2/Hand_0006322.jpg",
-        "/Users/yvtheja/Documents/Dataset2/Hand_0006323.jpg"
-    ]
-    Task3(10, imageDir, modelTypes=[ModelType.HOG],).visualiseSimilarImages(10, imagePaths)
+    # ImageIndex()
+    # imageDir = "/Users/yvtheja/Documents/Dataset2"
+    # imagePaths = [
+    #     "/Users/yvtheja/Documents/Dataset2/Hand_0006321.jpg",
+    #     "/Users/yvtheja/Documents/Dataset2/Hand_0006322.jpg",
+    #     "/Users/yvtheja/Documents/Dataset2/Hand_0006323.jpg"
+    # ]
+    # Task3(10, imageDir, modelTypes=[ModelType.HOG],).visualiseSimilarImages(10, imagePaths)
 
-    # for imagePath in glob.glob("/Users/yvtheja/Documents/Test_Set/*.jpg"):
-    #     print("Image class : {}".format(Task4PPRNoCache(CLASSIFICATION_IMG_DIR, CLASSIFICATION_META_CSV, modelTypes=[ModelType.CM]).getClassForImage(imagePath)))
+
+    # Run below code for pprClassification
+    for imagePath in glob.glob("/Users/yvtheja/Documents/Test_Set/*.jpg"):
+        print("Image class : {}".format(Task4PPRNoCache(CLASSIFICATION_IMG_DIR, CLASSIFICATION_META_CSV, modelTypes=[ModelType.CM]).getClassForImage(imagePath)))
 
 
 if __name__ == "__main__":
